8dc5571cc18f153bb7a2dc39e820b8f2b9a543f5
unknown <2514290017@ACADEMICO.IESB.BR>
2026-03-23 10:22:13 -0300

    atividade

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,40 @@
-import './style.css'
+const form = document.querySelector('#loginForm');
+const email = document.querySelector('#email');
+const senha = document.querySelector('#senha');
+const emailErro = document.querySelector('#emailError');
+const senhaErro = document.querySelector('#senhaError');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  emailErro.textContent = '';
+  senhaErro.textContent = '';
+
+  let formularioValido = true;
+
+  if (email.value.trim() === '') {
+    emailErro.textContent = 'O campo de email é obrigatório.';
+    formularioValido = false;
+  }
+
+  if (senha.value.trim() === '') {
+    senhaErro.textContent = 'O campo de senha é obrigatório.';
+    formularioValido = false;
+  }
+
+  if (formularioValido) {
+    window.location.href = 'dashboard.html';
+  }
+});
+
+email.addEventListener('input', function () {
+  if (email.value.trim() !== '') {
+    emailErro.textContent = '';
+  }
+});
+
+senha.addEventListener('input', function () {
+  if (senha.value.trim() !== '') {
+    senhaErro.textContent = '';
+  }
+});
